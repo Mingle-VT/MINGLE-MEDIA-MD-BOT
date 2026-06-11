05c441fb46816f39c788d997af5f67f42abbee50
Mingle <detectivemingle@gmail.com>
2026-06-11 22:01:14 +0000

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+    giphyApiKey: process.env.GIPHY_API_KEY || 'dc6zaTOxFJmzC',
+    ownerNumber: process.env.OWNER_NUMBER || '233530347197',
+    botName: 'MINGLE-MEDIA MD-BOT',
+    ownerName: 'MINGLE-MEDIA'
+};
